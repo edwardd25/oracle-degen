27c897b9c45951139a09a577a79a8286001cb400
edwardd25 <kurakuro25@gmail.com>
2025-09-30 14:31:19 +0700

    Update Page

--- app/page.tsx
+++ app/page.tsx
@@ -158,7 +158,7 @@ export default function Home() {
             Twitter/X
           </a>
           <a 
-            href="https://oracle-degen.gitbook.io/oracle-degen-docs/" 
+            href="https://tally.so/r/mRp9vP" 
             target="_blank" 
             rel="noopener noreferrer"
             style={{
@@ -176,7 +176,7 @@ export default function Home() {
             <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
               <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8l-6-6zm4 18H6V4h7v5h5v11z"/>
             </svg>
-            Docs
+            Docs (Coming soon)
           </a>
         </div>
       </div>
